6e8bd62b9fda38ae6e91d0fe06dc65361dc41959
nr33 <nivetha.r@dxc.com>
2019-04-14 18:31:07 +0530

    added prettier

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/map';
-import {ContactDetailsService} from '../_service/contact-details.service';
+import { ContactDetailsService } from '../_service/contact-details.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private myService:ContactDetailsService,private router: Router) { }
-  contacts:any;
+  constructor(
+    private myService: ContactDetailsService,
+    private router: Router
+  ) {}
+  contacts: any;
   clickedIndex: any;
 
   ngOnInit() {
-    
-    this.myService.getContacts().subscribe(data  =>  {
-      this.contacts =data;
+    this.myService.getContacts().subscribe(data => {
+      this.contacts = data;
       console.log(JSON.stringify(data, null, 2));
     });
   }
   displayDetails(i) {
     this.clickedIndex = i;
   }
-  onSelect(contact){
-    console.log("contact is",contact.id)
-    this.router.navigate(['/details',contact.id]);
+  onSelect(contact) {
+    console.log('contact is', contact.id);
+    this.router.navigate(['/details', contact.id]);
   }
-
 }
