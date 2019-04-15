663ab8616ceaa70737e44f9d08533273caa001d3
nivethar12 <nivetha.r12@gmail.com>
2019-04-16 00:10:32 +0530

    Lot of UI Cleanups

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,46 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {ContactDetailsService} from '../_service/contact-details.service';
-import { ActivatedRoute,Router} from '@angular/router';
+import { ContactDetailsService } from '../_service/contact-details.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ParamMap } from '@angular/router';
-
 
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrls: ['./contact-detail.component.css']
+  styleUrls: ['./contact-detail.component.css'],
 })
 export class ContactDetailComponent implements OnInit {
-
-  constructor(private myService:ContactDetailsService,private route: ActivatedRoute,private router: Router) { }
-  details:any;
-  currentContact:any;
+  constructor(
+    private myService: ContactDetailsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  details: any;
+  currentContact: any;
   public contactId;
   ngOnInit() {
-    
-    // let id= parseInt(this.route.snapshot.paramMap.get('id'));
-    // this.contactId=id;
-    this.route.paramMap.subscribe((params : ParamMap) => {
-        let id= parseInt(params.get('id'));
-        this.contactId=id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.contactId = id;
     });
-    this.myService.getContactDetails(this.contactId).subscribe(data  =>  {
-      this.details =data;
-      console.log(JSON.stringify(this.details, null, 2));
-      // this.currentContact=this.details[this.contactId];
-      this.currentContact=this.details;
+    this.myService.getContactDetails(this.contactId).subscribe(data => {
+      this.currentContact = data;
     });
   }
-  deleteContact(){
-  var userInput = confirm("Are yoy sure,you want to delete?");
-  if (userInput == true) {
-   
-    console.log("delete");
-  } else {
-    
-    console.log("ignore it");
+  addToFav() {
+    this.myService.addFav({ id: this.contactId }).subscribe(data => {
+      this.ngOnInit();
+    });
   }
-}
-editContacts(){
-  this.router.navigate(['/edit',this.contactId]);
-}
+  removeFromFav() {
+    this.myService.removeFav({ id: this.contactId }).subscribe(data => {
+      this.ngOnInit();
+    });
+  }
+  deleteContact() {
+    var userInput = confirm('Are you sure,you want to delete?');
+    if (userInput == true) {
+      this.myService.delete({ id: this.contactId }).subscribe(data => {
+        this.router.navigate(['/']);
+      });
+    }
+  }
+  editContacts() {
+    this.router.navigate(['/edit', this.contactId]);
+  }
 }
